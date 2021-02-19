80ddfd12c860ab61d6c6c8bb80ff40fd98ee3ed7
lifeflavor0425 <rjsdnakfmzh61@gmail.com>
2021-02-19 05:43:46 +0000

    2.11 강의

--- indeed.py
+++ indeed.py
@@ -2,10 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 LIMIT=50
 URL=f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
-def extract_indeed_pages():
+def get_last_jobs():
   
-  resul=requests.get(URL)
-  soup=BeautifulSoup(resul.text ,"html.parser")
+  result=requests.get(URL)
+  soup=BeautifulSoup(result.text ,"html.parser")
   pagination=soup.find("div",{"class":"pagination"})
 
   links=pagination.find_all('a')
@@ -16,15 +16,20 @@ def extract_indeed_pages():
   max_page=pages[-1]
   return max_page
 
-def extract_jobs(html):
+def extract_job(html):
     title = html.find("h2",{"class":"title"}).find("a")["title"]
     company= html.find("span",{"class":"company"})
-    company_anchor=company.find("a")
-    if company_anchor is not None:
-     company=str(company_anchor.string)
+
+    if company :
+      company_anchor=company.find("a")
+      if company_anchor is not None:
+        company=str(company_anchor.string)
+      else :
+        company=str(company.string)
+      company=company.strip()
     else :
-      company=str(company.string)
-    company=company.strip()
+      company=None
+    
     location= html.find("div",{"class":"recJobLoc"})["data-rc-loc"]
     job_id=html["data-jk"]
     
@@ -35,7 +40,7 @@ def extract_jobs(html):
       "link":f"https://www.indeed.com/viewjob?jk={job_id}"
     }
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
   jobs=[]
   for page in range(last_page):
       print(f"Scrapping page {page}")
@@ -43,8 +48,12 @@ def extract_indeed_jobs(last_page):
       soup=BeautifulSoup(result.text, "html.parser")
       results=soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
       for result in results :
-        job=extract_jobs(result)
+        job=extract_job(result)
         jobs.append(job)
 
   return jobs
     
+def get_jobs() :
+  last_page=get_last_jobs()
+  jobs=extract_jobs(last_page)
+  return jobs
